df7412c2607df9c84755fbf381ed771c3b0f194c
Matthew Parker <matt_parker@outlook.com>
2026-05-17 16:54:01 -0400

    perf: audit vitals and update flow

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portfolio-v9';
+const CACHE = 'portfolio-v10';
 const PRECACHE = ['/', '/manifest.json', '/favicon.svg', '/apple-touch-icon.png', '/icon-192.png', '/icon-512.png', '/rss.xml'];
 const FETCH_TIMEOUT = 10000;
 
@@ -29,13 +29,17 @@ async function cachedOrOffline(request, fallbackPath) {
 }
 
 self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE).then((c) => c.addAll(PRECACHE)).then(() => self.skipWaiting()));
+    e.waitUntil(caches.open(CACHE).then((c) => c.addAll(PRECACHE)));
 });
 
 self.addEventListener('activate', (e) => {
     e.waitUntil(
         caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())
     );
+});
+
+self.addEventListener('message', (e) => {
+    if (e.data && e.data.type === 'SKIP_WAITING') self.skipWaiting();
 });
 
 self.addEventListener('fetch', (e) => {
